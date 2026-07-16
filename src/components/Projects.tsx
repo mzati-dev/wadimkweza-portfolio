0b6eb93a301ea62f5f539e1710fc9bea8a6395fe
Wadi Mkweza <wadimkweza@gmail.com>
2026-07-16 18:41:14 +0200

    Adjust grid layout for project cards

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -102,7 +102,7 @@ const Projects: React.FC = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
           {projects.map((project, index) => (
             <ProjectCard key={index} {...project} />
           ))}
